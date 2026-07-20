222ee01b0beb74ed2c1593325c794d3d92a81dbe
sy7701855-hub <sy7701855@gmail.com>
2026-07-20 22:31:32 +0530

    791A-bear and big brother

---
+++ 13_Bear_and_big_brother.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int count=1;
+    while(a<b){
+a*=3;
+b*=2;
+count++;
+    }
+    cout<<count;
+    return 0;
+}
