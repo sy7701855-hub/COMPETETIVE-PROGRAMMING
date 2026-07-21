dd097628a5675d4988356f79d805323aa8570ab6
sy7701855-hub <sy7701855@gmail.com>
2026-07-21 22:29:43 +0530

    Koshary

---
+++ 16_Koshary.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+    int x,y;
+    cin>>x>>y;
+    if(x%2==0 || y%2==0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;;
+    }
+}
+    return 0;
+}
