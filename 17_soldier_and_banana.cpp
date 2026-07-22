736755d6f532f181c560120838d26452077241f8
sy7701855-hub <sy7701855@gmail.com>
2026-07-22 19:14:27 +0530

    546A-soldier and banana

---
+++ 17_soldier_and_banana.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,n,w;
+    cin>>k>>n>>w;
+    int sum=0;
+    for(int i=1;i<=w;i++){
+     sum=sum+(i*k);
+    }
+    if(sum>n){
+        cout<<sum-n;
+    }else{
+        cout<<0;
+    }
+
+}
