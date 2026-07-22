f6a76c8ea997f212c44a136e26e0ec067a375736
sy7701855-hub <sy7701855@gmail.com>
2026-07-22 19:50:16 +0530

    59A-word

---
+++ 18_Word.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int upper=0;
+    int lower=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='A'&& s[i]<='Z'){
+            upper++;
+        }else{
+            lower++;
+        }
+    }
+    if(lower>=upper){
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        cout<<s;
+    }else{
+        transform(s.begin(),s.end(),s.begin(),::toupper);
+        cout<<s;
+    }
+    return 0;
+}
